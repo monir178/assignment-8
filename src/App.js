464105aa7130478882f22056d1ef5dd9575fceb0
monir178 <monir.mzs17@gmail.com>
2022-10-10 03:43:50 +0600

    tailwind linked

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import Header from './components/Header/Header';
 import Workouts from './components/Workouts/Workouts';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
       <Workouts></Workouts>
 
     </div>
--- src/components/Workouts/Workouts.js
+++ src/components/Workouts/Workouts.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import './Workout.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+
+
 
 const Workouts = () => {
+    const [workouts, setWorkouts] = useState([]);
+
+    useEffect(() => {
+        fetch('data.json')
+            .then(res => res.json())
+            .then(data => setWorkouts(data))
+    }, [])
     return (
-        <div className='workouts-container'>
+
+        <div className='workouts-container container mx-auto'>
+            <div className='header-name text-4xl'>
+                <h1><FontAwesomeIcon icon={faDumbbell} />  ULTRA-ACTIVE-CLUB</h1>
+            </div>
             <div>
                 <h3>Select today's exercise</h3>
             </div>
             <div>
-
+                {
+                    workouts.map(workout => console.log(workout))
+                }
             </div>
         </div>
     );
